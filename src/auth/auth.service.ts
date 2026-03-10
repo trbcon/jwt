2e909c8e9103f97ddff981459c882baa7054e076
trbcon <pahax.off@gmail.com>
2026-03-10 21:37:43 +0300

    first commit

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,31 @@
+import { ConflictException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { RegisterRequest } from './dto/register.dto';
+
+@Injectable()
+export class AuthService {
+    constructor(private readonly prismaService: PrismaService) {}
+
+    async register(dto: RegisterRequest) {
+        const { name, email, password } = dto;
+
+        const existUser = await this.prismaService.user.findUnique({
+            where: {
+                email,
+            },
+        });
+
+        if (existUser) {
+            throw new ConflictException('Почта есть')
+        }
+
+        const user = await this.prismaService.user.create({
+            data: {
+                name, email, password
+            }
+        })
+
+        return user;
+    }
+
+}
